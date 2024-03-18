d4dc4ae72530c6b7f91818ae7b50222294fa73c0
whaley <cwhaley112@gmail.com>
2024-03-18 10:27:36 -0400

    glfw has put Serious strain on my relationship with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,105 @@
+// #include <iostream>
+// #include "../lib/glfw/include/GLFW/glfw3.h"
+// #include "Util/MathUtil.h"
+// #include "Util/Vector.h"
+//
+// using namespace whal;
+//
+// int main(int argc, char* argv[]) {
+//     std::cout << "running..." << std::endl;
+//
+//     Vector2f myVec1(5.0, 5.0);
+//     Vector2f myVec2(-5.0, -5.0);
+//     // f32 dot = vec1.dot(vec2);
+//     f32 out = abs(myVec1.dot(myVec2));
+//     // f32 out = vec1.dot(vec2);
+//     // f32 out = vec1.len();
+//     // f32 out = abs(-1 * Vector::zero2f.len());
+//     std::cout << out << std::endl;
+//
+//     return 0;
+// }
+
 #include <iostream>
-#include "Util/MathUtil.h"
-#include "Util/Vector.h"
 
-using namespace whal;
+// GLAD
+#include <glad/gl.h>
+// #include "../lib/glad/include/glad/gl.h"
 
-int main(int argc, char* argv[]) {
-    std::cout << "running..." << std::endl;
+// GLFW (include after glad)
+#include <GLFW/glfw3.h>
+// #include "../lib/glfw/include/GLFW/glfw3.h"
 
-    Vector2f vec1(5.0, 5.0);
-    Vector2f vec2(-5.0, -5.0);
-    // f32 dot = vec1.dot(vec2);
-    f32 out = abs(vec1.dot(vec2));
-    // f32 out = vec1.dot(vec2);
-    // f32 out = vec1.len();
-    // f32 out = abs(-1 * Vector::zero2f.len());
-    std::cout << out << std::endl;
+// This example is taken from http://learnopengl.com/
+// http://learnopengl.com/code_viewer.php?code=getting-started/hellowindow2
+// The code originally used GLEW, I replaced it with Glad
 
+// Compile:
+// g++ example/c++/hellowindow2.cpp -Ibuild/include build/src/gl.c -lglfw -ldl
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// The MAIN function, from here we start the application and run the game loop
+int main() {
+    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    // Init GLFW
+    glfwInit();
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    // Load OpenGL functions, gladLoadGL returns the loaded version, 0 on error.
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    // Successfully loaded OpenGL
+    std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
+
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window)) {
+        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminates GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
     return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    // if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    //     glfwSetWindowShouldClose(window, GL_TRUE);
 }
